f31a4a1dc78dd23b340e828ab74407a763c758ed
sunnyegg <adilatresna21@gmail.com>
2025-05-19 10:45:24 +0700

    implement hashing for encryption keys

--- src/chainvault_frontend/src/lib/crypto.ts
+++ src/chainvault_frontend/src/lib/crypto.ts
@@ -35,3 +35,14 @@ export const decodeBase64 = (data: string) => {
   }
   return new TextDecoder().decode(bytes);
 };
+
+export const hash256 = async (data: string) => {
+  const encoder = new TextEncoder();
+  const dataBuffer = encoder.encode(data);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", dataBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((byte) => byte.toString(16).padStart(2, "0"))
+    .join("");
+  return hashHex;
+};
